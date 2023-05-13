882615ab2cc8776113b9a9732e69413dafa0f215
Pieta9221 <mercy_nwaodu@yahoo.com>
2023-05-13 22:21:45 +0100

    build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  base: 'https://pieta9221.github.io/React-login/'
 })
